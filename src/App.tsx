dce71f95909df9c76e96ba9f5e7cdbdf47d54593
William Huang <72718676+imaginaryg00se@users.noreply.github.com>
2026-03-03 18:44:26 -0500

    Initial ThinkingType MVP scaffold

---
+++ src/App.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from "react";
+
+function App() {
+  const passage = 
+    "Your identity is something you can update at any time. Your identity dictates how you experience the game.";
+
+  const [input, setInput] = useState("");
+
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [endTime, setEndTime] = useState<number | null>(null);
+
+  const [totalKeyStrokes, setTotalKeyStrokes] = useState(0);
+
+  // Compare passage with user input and count correct inputs
+  const correctChars = passage
+    .split("")
+    .reduce((count, char, index) => count + (input[index] === char ? 1 : 0), 0);
+
+  // The win condition
+  const isComplete = 
+    input.length === passage.length && 
+    correctChars === passage.length;
+
+  useEffect(() => {
+    // Start timer on first character
+    if (input.length > 0 && startTime === null) {
+      setStartTime(Date.now());
+    }  
+
+    // Stop timer when complete (only once) 
+    if (isComplete && endTime === null) {
+      setEndTime(Date.now());
+    }
+  }, [input.length, isComplete, startTime, endTime]);
+
+  const elapsedMs = 
+    startTime === null ? 0 : (endTime ?? Date.now()) - startTime; 
+
+  const elapsedSeconds = Math.floor(elapsedMs / 1000);
+
+  const accuracy = 
+    totalKeyStrokes === 0
+      ? 0
+      : (passage.length / totalKeyStrokes) * 100;
+
+  const elapsedMinutes = elapsedMs / 1000 / 60; 
+  const wpm = 
+    elapsedMinutes > 0 ? (correctChars / 5) / elapsedMinutes : 0;
+
+  const reset = () => {
+    setInput("");
+    setStartTime(null);
+    setEndTime(null);
+    setTotalKeyStrokes(0);
+  }
+
+
+  return (
+    <div>
+      <h1>ThinkingType</h1>
+      <p>
+        {passage.split("").map((char, index) => { 
+          let color = "black"; 
+
+          if (index < input.length) {
+            color = input[index] === char ? "green" : "red"
+          };
+
+          return (
+            <span key={index} style={{ color }} >
+              {char}
+            </span>
+          )
+          
+      })}
+      </p>
+
+      <input
+        value={input}
+        onChange={(e) => {
+          setTotalKeyStrokes((prev) => prev + 1);
+          setInput(e.target.value)}
+        }
+        disabled={isComplete}
+      />
+
+      {isComplete && <p>Completed</p>}
+
+      {isComplete && (
+        <>
+        <p>Accuracy: {accuracy.toFixed(1)}%</p>
+        <p>WPM: {wpm.toFixed(1)}</p>
+        </>
+      )}
+
+      {isComplete && (
+        <button onClick={reset}>
+          Restart
+        </button>
+      )}
+
+      <p>Time: {elapsedSeconds}s</p>
+
+      <p>Characters typed: {input.length}</p>
+    </div>
+  );
+}
+
+export default App;
